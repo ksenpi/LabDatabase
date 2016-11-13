34fad5f7df3e515ca0e4ab49021ca6525e667c02
Darius <o1i0b@ugrad.cs.ubc.ca>
2016-11-12 21:36:26 -0800

    Lab Manager Queries

--- 304DatabaseProject/src/mainViews/LabManager.java
+++ 304DatabaseProject/src/mainViews/LabManager.java
@@ -28,17 +28,45 @@ public class LabManager extends Application implements User{
     @Override
     public void start(Stage primaryStage) {
 
-        System.out.println("we got here!");
-
+        System.out.println("Boxless Samples: ");
         Map<String, String> boxless = findBoxlessSamples();
+//        if (boxless == null) {
+//            System.out.println("boxless is empty somehow ");
+//        }
         for (String key : boxless.keySet()) {
-            System.out.println(key + "  ,  " + boxless.get(key).toString());
+            System.out.println(key + "  ,  " + boxless.get(key));
         }
 
+        System.out.println("UnderCapacity: ");
         Map<String, String[]> notCapacity = findContainersUnderCapacity();
         for (String key : notCapacity.keySet()) {
             System.out.printf("%s", key);
             for(String element: notCapacity.get(key)){
+                System.out.printf("%s", "     ,     ");
+                System.out.printf("%s", element);
+            }
+            System.out.printf("%s\n", "");
+
+        }
+
+        System.out.println("Work List: ");
+        Map<String, String[]> workerList = generateWorkList();
+
+        for (String key : workerList.keySet()) {
+            System.out.printf("%s", key);
+            for(String element: workerList.get(key)){
+                System.out.printf("%s", "     ,     ");
+                System.out.printf("%s", element);
+            }
+            System.out.printf("%s\n", "");
+
+        }
+
+        System.out.println("Sample List: ");
+        Map<String, String[]> sampleList = generateSampleList();
+        for (String key : sampleList.keySet()) {
+            System.out.printf("%s", key);
+            for(String element: sampleList.get(key)){
                 System.out.printf("%s", "     ,     ");
                 System.out.printf("%s", element);
             }
@@ -310,9 +338,9 @@ public class LabManager extends Application implements User{
                         "contains c on s.samp_id = c.samp_id where c.c_id IS NULL order by s.samp_id");
                 while (results.next()) {
                     //Get the values at each row
-                    sampleID = results.getString("samp_id");
-                    dateCreated = results.getDate("date_cr").toString();
                     if (!results.wasNull()) {
+                    sampleID = "Sample ID: " + results.getString("samp_id");
+                    dateCreated = "Date Created: " + results.getDate("date_cr").toString();
                         boxlessSamples.put(sampleID, dateCreated);
                     }
                 }
@@ -348,7 +376,7 @@ public class LabManager extends Application implements User{
                         "on c2.c_occupancy=c1.c_occupancy where c1.at_capacity=0 AND c2.f_occupancy " +
                         "IS NOT NULL");
                 while (results.next()) {
-                    containerID = results.getString("c_id");
+                    containerID = "Container ID: " + results.getString("c_id");
                     fridgeID = results.getString("fr_id");
                     fridgeOccupancy = results.getString("f_occupancy");
                     containerOccupancy = results.getString("c_occupancy");
@@ -371,19 +399,204 @@ public class LabManager extends Application implements User{
     }
 
     @Override
-    //Query
-    //TODO (Darius): Refer to the application functionality document in the Drive. This one is similar
+    //Query 12
     //To the query in ExternalUser, but this workList generates employee IDs as well.
     public Map<String, String[]> generateWorkList() {
+        Statement stmt1;
+        Statement stmt2;
+        ResultSet results;
+        OurConnection connectionToDatabase = new OurConnection();
+        if (connectionToDatabase.connect("ora_o1i0b", "a30662143")) {
+            //if (connectionToDatabase.connect("ora_e5w9a", "a10682145")) {
+            try {
+                Connection con = connectionToDatabase.getConnection();
+                stmt1 = con.createStatement();
+                stmt2 = con.createStatement();
+                String name;
+                String EmployeeID;
+                Map<String, String[]> workerList = new HashMap<String, String[]>();
+
+                results = stmt1.executeQuery("select name, emp_id from lab_manager");
+                while (results.next()) {
+                    name = results.getString("name");
+                    EmployeeID = "EmployeeID: " + results.getString("emp_id");
+                    if (!results.wasNull()) {
+                        String[] workerAttributes = {"Name: " + name, "Type: Lab Manager"};
+                        workerList.put(EmployeeID, workerAttributes);
+                    }
+                }
+                stmt1.close();
+
+                results = stmt2.executeQuery("select name, emp_id from researcher");
+                while (results.next()) {
+                    name = results.getString("name");
+                    EmployeeID = "EmployeeID: " + results.getString("emp_id");
+                    if (!results.wasNull()) {
+                        if (workerList.get(EmployeeID) == null) {
+                            String[] workerAttributes = {"Name: " + name, "Type: Researcher"};
+                            workerList.put(EmployeeID, workerAttributes);
+                        } else {
+                            String[] workerAttributes = {"Name: " + name, "Type: Lab Manager and Researcher"};
+                            workerList.put(EmployeeID, workerAttributes);
+                        }
+                    }
+                }
+                stmt1.close();
+                return workerList;
+            } catch (SQLException ex) {
+                System.out.println("Message: " + ex.getMessage());
+            }
+        }
         return null;
     }
 
     @Override
-    //Query
+    //Query 13
     //TODO (Darius): Refer to the application functionality document in the Drive. Again, this one is similar
     //To the query in ExternalUser, but this generates more information about the sample, which may
     //require more complex querying (such as details about the sample).
     public Map<String, String[]> generateSampleList() {
+        Statement stmt;
+        ResultSet results;
+        OurConnection connectionToDatabase = new OurConnection();
+        if (connectionToDatabase.connect("ora_o1i0b", "a30662143")) {
+            //if (connectionToDatabase.connect("ora_e5w9a", "a10682145")) {
+            try {
+                Connection con = connectionToDatabase.getConnection();
+                stmt = con.createStatement();
+                String sampleID;
+                String strain;
+                String volume;
+                String composition;
+                String concentration;
+                String name;
+                String antibiotic;
+                String res_enz_1;
+                String res_enz_2;
+                String origin;
+                String part1;
+                String part2;
+                String empID;
+                String dateCreated;
+                Map<String, String[]> sampleList = new HashMap<String, String[]>();
+
+                results = stmt.executeQuery("select s.samp_id, s.date_cr, p1.composition, b.strain, d1.concentration, " +
+                        "p2.name, p2.antibiotic, d2.res_enz_1, d2.res_enz_2, g1.origin, l.part1, l.part2, " +
+                        "r.emp_id, g2.volume from sample s LEFT JOIN plate p1 on s.samp_id = p1.sample_id " +
+                        "LEFT JOIN bacterial_culture b on s.samp_id = b.sample_id LEFT JOIN dna_sample d1 on " +
+                        "s.samp_id = d1.sample_id LEFT JOIN plasmid p2 on s.samp_id = p2.sample_id LEFT JOIN " +
+                        "digest d2 on s.samp_id = d2.sample_id LEFT JOIN genomic g1 on s.samp_id = g1.sample_id " +
+                        "LEFT JOIN ligation l on s.samp_id = l.sample_id LEFT JOIN researches r on s.samp_id = " +
+                        "r.samp_id LEFT JOIN glycerol_stock g2 on s.samp_id = g2.sample_id");
+                while (results.next()) {
+                    //All the attributes
+                    name = "Name: " + results.getString("name");
+                    sampleID = "SampleID: " + results.getString("samp_id");
+                    volume = "Volume: " + results.getString("volume");
+                    composition = "Composition: " + results.getString("composition");
+                    strain = "Strain: " + results.getString("strain");
+                    concentration = "Concentration: " + results.getString("concentration");
+                    antibiotic = "Antibiotic: " + results.getString("antibiotic");
+                    res_enz_1 = "Restriction Enzyme 1: " + results.getString("res_enz_1");
+                    res_enz_2 = "Restriction Enzyme 2: " + results.getString("res_enz_2");
+                    origin = "Origin: " + results.getString("origin");
+                    part1 = "Part1: " + results.getString("part1");
+                    part2 = "Part2: " + results.getString("part2");
+                    empID = "Researcher ID: " + results.getString("emp_id");
+                    dateCreated = "Date Created: " + results.getString("date_cr");
+                    if (empID != null) {
+                        // There is someone resarching the sample
+                        if (part1 != null) {
+                            String[] sampleAttributes = {empID, dateCreated, concentration, part1, part2};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (origin != null) {
+                            String[] sampleAttributes = {empID, dateCreated, concentration, origin};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (res_enz_1 != null) {
+                            String[] sampleAttributes = {empID, dateCreated, concentration, res_enz_1, res_enz_2};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (name != null) {
+                            String[] sampleAttributes = {empID, dateCreated, concentration, name, antibiotic};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (res_enz_1 != null) {
+                            String[] sampleAttributes = {empID, dateCreated, concentration, res_enz_1, res_enz_2};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (concentration != null) {
+                            String[] sampleAttributes = {empID, dateCreated, concentration};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (composition != null) {
+                            String[] sampleAttributes = {empID, dateCreated, strain, composition};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (volume != null) {
+                            String[] sampleAttributes = {empID, dateCreated, strain, volume};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (strain != null) {
+                            String[] sampleAttributes = {empID, dateCreated, strain};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (sampleID != null) {
+                            String[] sampleAttributes = {empID, dateCreated};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                    } else //There is nobody researching the sample
+                    {
+                        if (part1 != null) {
+                            String[] sampleAttributes = {dateCreated, concentration, part1, part2};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (origin != null) {
+                            String[] sampleAttributes = {dateCreated, concentration, origin};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (res_enz_1 != null) {
+                            String[] sampleAttributes = {dateCreated, concentration, res_enz_1, res_enz_2};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (name != null) {
+                            String[] sampleAttributes = {dateCreated, concentration, name, antibiotic};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (res_enz_1 != null) {
+                            String[] sampleAttributes = {dateCreated, concentration, res_enz_1, res_enz_2};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (concentration != null) {
+                            String[] sampleAttributes = {dateCreated, concentration};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (composition != null) {
+                            String[] sampleAttributes = {dateCreated, strain, composition};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (volume != null) {
+                            String[] sampleAttributes = {dateCreated, strain, volume};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (strain != null) {
+                            String[] sampleAttributes = {dateCreated, strain};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                        else if (sampleID != null) {
+                            String[] sampleAttributes = {dateCreated};
+                            sampleList.put(sampleID, sampleAttributes);
+                        }
+                    }
+                }
+                stmt.close();
+                return sampleList;
+
+            } catch (SQLException ex) {
+                System.out.println("Message: " + ex.getMessage());
+            }
+        }
         return null;
     }
 }
